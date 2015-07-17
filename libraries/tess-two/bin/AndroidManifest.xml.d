fdbcebaeb1e309f3088728667425a639ccbc8fef
ILikeCheese13 <jacobsteel308@gmail.com>
2015-07-17 17:18:49 -0400

    Initial commit

---
+++ libraries/tess-two/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Biolux-DEV\Downloads\tess-two-master\tess-two-master\tess-two\bin\AndroidManifest.xml : \
+C:\Users\Biolux-DEV\Downloads\tess-two-master\tess-two-master\tess-two\AndroidManifest.xml \
